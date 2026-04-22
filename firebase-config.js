b64c08f9fb6622e6932805052dcc878437a1d3be
joedip16 <joedippolito16@gmail.com>
2026-04-22 13:50:16 -0400

    Enable Firebase App Check

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 // Paste your Firebase web app config here, then set firebaseSyncEnabled to true.
 // You can find this in Firebase Console > Project settings > Your apps > Web app.
 window.firebaseSyncEnabled = true;
-window.firebaseAppCheckEnabled = false;
-window.firebaseAppCheckSiteKey = "PASTE_RECAPTCHA_OR_ENTERPRISE_SITE_KEY";
+window.firebaseAppCheckEnabled = true;
+window.firebaseAppCheckSiteKey = "6LfK98QsAAAAAI9_Ji69Hs9rn8icMAX7K6c8c0rN";
 window.firebaseAppCheckDebugToken = false;
 
 window.firebaseConfig = {
